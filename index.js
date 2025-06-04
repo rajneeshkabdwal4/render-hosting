158b5812df6300e306f4600e0327053d44730639
Rajneesh Kabdwal <95100012+rajneeshkabdwal4@users.noreply.github.com>
2025-06-04 09:42:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,6 @@ import axios from "axios";
 
 const app = express();
 
-const url = `https://render-hosting-se2b.onrender.com`;
-const interval = 30000;
-
-function reloadWebsite() {
-  axios
-    .get(url)
-    .then((response) => {
-      console.log("website reloded");
-    })
-    .catch((error) => {
-      console.error(`Error : ${error.message}`);
-    });
-}
-
-setInterval(reloadWebsite, interval);
 
 app.get("/", (req, res) => {
   res.send("hello world");
